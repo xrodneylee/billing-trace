44633fe706cac48230bc05e57d35f248830f28ee
xrodneylee <xrodneylee@gmail.com>
2017-07-28 00:10:18 +0800

    pymongo + altas

--- app.py
+++ app.py
@@ -3,8 +3,23 @@
 
 from flask import Flask
 from flask_restful import Api
-from flask_pymongo import PyMongo
+import pymongo
 
 app = Flask(__name__)
-mongo = PyMongo(app)
 api = Api(app)
+
+client = pymongo.MongoClient("mongodb://xrodneylee:xrodneylee@cluster0-shard-00-00-5ekni.mongodb.net:27017,cluster0-shard-00-01-5ekni.mongodb.net:27017,cluster0-shard-00-02-5ekni.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin")
+db = client.lab
+collection = db.test_collection
+
+
+# for i in range(20):
+#     post = {"author": "Mike", \
+#     "text": "My first blog post!",\
+#     "tags": ["mongodb", "python", "pymongo"],\
+#     "date": datetime.datetime.utcnow(), \
+#     "aa": i}
+#     post_id = collection.insert_one(post).inserted_id
+#     print(post_id)
+if __name__ == '__main__':
+    app.run(debug=True)
