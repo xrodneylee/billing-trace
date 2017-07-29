44cf447ab111350054dc75bfdada0dc602add8df
xrodneylee <xrodneylee@gmail.com>
2017-07-29 17:29:20 +0800

    lint

--- app/services/azure.py
+++ app/services/azure.py
@@ -12,16 +12,16 @@ class AzureCredential:
         """
         url = 'https://login.microsoftonline.com/' + self.tenant + '/oauth2/token'
         header = {
-            'Content-Type' : 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
         params = {
-            'api-version' : '1.0'
+            'api-version': '1.0'
         }
         data = {
-            'grant_type' : 'client_credentials',
-            'resource' : 'https://management.core.windows.net/',
-            'client_id' : self.client_id,
-            'client_secret' : self.client_secret
+            'grant_type': 'client_credentials',
+            'resource': 'https://management.core.windows.net/',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret
         }
 
         response = requests.post(url, params=params, headers=header, data=data)
@@ -44,15 +44,15 @@ class AzureUsage():
                 subscriptions/08baa038-b64f-49f0-a084-7c22d1c1305c/ \
                 providers/Microsoft.Commerce/UsageAggregates'
         header = {
-            'Content-Type' : 'application/json',
-            'Authorization' : 'Bearer ' + token
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
         }
         params = {
-            'api-version' : '2015-06-01-preview',
-            'reportedStartTime' : '2017-03-22T00%3a00%3a00%2b00%3a00',
-            'reportedEndTime' : '2017-03-23T00%3a00%3a00%2b00%3a00',
-            'aggregationGranularity' : 'Hourly',
-            'showDetails' : 'true'
+            'api-version': '2015-06-01-preview',
+            'reportedStartTime': '2017-03-22T00%3a00%3a00%2b00%3a00',
+            'reportedEndTime': '2017-03-23T00%3a00%3a00%2b00%3a00',
+            'aggregationGranularity': 'Hourly',
+            'showDetails': 'true'
         }
 
         response = requests.post(url, params=params, headers=header)
