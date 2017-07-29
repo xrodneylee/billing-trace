3d70d243f8e40cd062be8370702c7b50e69f7d69
xrodneylee <xrodneylee@gmail.com>
2017-07-29 23:37:03 +0800

    azure billing module

--- app/services/azure.py
+++ app/services/azure.py
@@ -30,39 +30,69 @@ class AzureCredential:
 
 class AzureUsage():
 
-    def __init__(self, tenant=None, client_id=None, client_secret=None):
-        self.tenant = tenant
-        self.client_id = client_id
-        self.client_secret = client_secret
+    def __init__(self, token=None, subscription=None):
+        self.token = token
+        self.subscription = subscription
 
     def invoke(self):
-
-        azure_credential = AzureCredential(self.tenant, self.client_id, self.client_secret)
-        token = azure_credential.invoke().json()['access_token']
-
-        url = 'https://management.azure.com/ \
-                subscriptions/08baa038-b64f-49f0-a084-7c22d1c1305c/ \
-                providers/Microsoft.Commerce/UsageAggregates'
+        url = 'https://management.azure.com/subscriptions/' \
+                + self.subscription \
+                + '/providers/Microsoft.Commerce/UsageAggregates'
         header = {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
+            'Authorization': 'Bearer ' + self.token
         }
         params = {
             'api-version': '2015-06-01-preview',
-            'reportedStartTime': '2017-03-22T00%3a00%3a00%2b00%3a00',
-            'reportedEndTime': '2017-03-23T00%3a00%3a00%2b00%3a00',
+            'reportedStartTime': '2017-03-22T00:00:00+00:00',
+            'reportedEndTime': '2017-03-23T00:00:00+00:00',
             'aggregationGranularity': 'Hourly',
             'showDetails': 'true'
         }
 
-        response = requests.post(url, params=params, headers=header)
+        response = requests.get(url, params=params, headers=header)
 
         return response
 
 class AzureRatecard():
 
-    def get(self):
-        return 'guanpu'
+    def __init__(self, token=None, subscription=None, offer_durable_id=None,
+                 currency='TWD', locale='zh-TW', region_info='TW'):
+        self.token = token
+        self.subscription = subscription
+        self.offer_durable_id = offer_durable_id # MS-AZR-0025P
+        self.currency = currency
+        self.locale = locale
+        self.region_info = region_info
 
-    def post(self):
-        pass
+    def invoke(self):
+        url = 'https://management.azure.com/subscriptions/' \
+                + self.subscription \
+                + '/providers/Microsoft.Commerce/RateCard'
+        header = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + self.token
+        }
+        params = {
+            'api-version': '2016-08-31-preview',
+            '$filter': "OfferDurableId eq '{offer_durable_id}' \
+                        and Currency eq '{currency}' \
+                        and Locale eq '{locale}' \
+                        and RegionInfo eq '{region_info}' " \
+            .format(
+                offer_durable_id=self.offer_durable_id,
+                currency=self.currency,
+                locale=self.locale,
+                region_info=self.region_info
+            )
+        }
+
+        response = requests.get(url, params=params, headers=header)
+
+        return response
+
+
+# test
+credential = AzureCredential('472613e3-303b-4ae2-afc6-6a3b2d920675', 'e4ec2c26-7b68-4b31-9665-b6cdeceb78a3', 'tD0HJEdnovFZ9ytAINVsDZnriebhLZuGtrv46W2y0g8=')
+ratecard = AzureRatecard(credential.invoke().json()['access_token'], '08baa038-b64f-49f0-a084-7c22d1c1305c', 'MS-AZR-0025P')
+print(ratecard.invoke().text)
