25200561a48d8afe7e6f91507d986507662126fc
xrodneylee <xrodneylee@gmail.com>
2017-07-31 23:35:00 +0800

    import data init

---
+++ app/services/import_data.py
@@ -0,0 +1,10 @@
+from .azure import AzureCredential, AzureRatecard, AzureUsage
+# from config import db
+# from bson.json_util import dumps
+
+# tenants = {}
+# subscriptions = {}
+
+# print(dumps(db.tenant.find()))
+
+credential = AzureCredential()
