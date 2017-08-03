60227aa9259d7b475ed01a54ec00e1e4c0b1362a
xrodneylee <xrodneylee@gmail.com>
2017-08-03 21:49:42 +0800

    role module

---
+++ app/resources/role.py
@@ -0,0 +1,30 @@
+from flask_restful import Resource
+from config import db
+
+class Admin(Resource):
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def put(self):
+        pass
+
+class User(Resource):
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def put(self):
+        pass
