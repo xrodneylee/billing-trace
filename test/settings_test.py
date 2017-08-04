f496d868c3619ee3940ba82884ead4fb7112b8c2
xrodneylee <xrodneylee@gmail.com>
2017-08-05 00:56:21 +0800

    [test] settings_test : TenantTestCase : : initial

---
+++ test/settings_test.py
@@ -0,0 +1,25 @@
+import unittest
+import tempfile
+
+class TenantTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_tenant_post(self):
+        pass
+
+    def test_tenant_get(self):
+        pass
+
+    def test_tenant_put(self):
+        pass
+
+    def test_tenant_delete(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
