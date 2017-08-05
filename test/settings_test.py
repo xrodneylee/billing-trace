23e413c2a782ced395a7d207d8fde8a03cfe9e6e
xrodneylee <xrodneylee@gmail.com>
2017-08-05 20:00:33 +0800

    [test] setting_test : TenantTestCase : : unittest

--- test/settings_test.py
+++ test/settings_test.py
@@ -9,27 +9,32 @@ class TenantTestCase(unittest.TestCase):
         self.header = {
             'Content-Type': 'application/json'
             }
-        data = {
+
+        self.data = {
             "tenant": "mock",
+            "client_id": "mockId",
+            "client_secret": "mockPasswrod"
+        }
+        response = requests.post(self.base_url, headers=self.header, json=self.data)
+
+    def tearDown(self):
+        url = self.base_url + "/mock"
+        response = requests.delete(url, headers=self.header)
+
+    def test_tenant_post(self):
+        data = {
+            "tenant": "mock1",
             "client_id": "mockId",
             "client_secret": "mockPasswrod"
         }
         response = requests.post(self.base_url, headers=self.header, json=data)
 
-    def tearDown(self):
-        pass
+        self.assertEqual(json.loads(response.json())['tenant'], 'mock1')
+        self.assertEqual(json.loads(response.json())['client_id'], 'mockId')
+        self.assertEqual(json.loads(response.json())['client_secret'], 'mockPasswrod')
 
-    def test_tenant_post(self):
-        data = {
-            "tenant": "guanpu",
-            "client_id": "id",
-            "client_secret": "passwrod"
-        }
-        response = requests.post(self.base_url, headers=self.header, json=data)
-
-        self.assertEqual(json.loads(response.json())['tenant'], 'guanpu')
-        self.assertEqual(json.loads(response.json())['client_id'], 'id')
-        self.assertEqual(json.loads(response.json())['client_secret'], 'passwrod')
+        url = self.base_url + "/mock1"
+        response = requests.delete(url, headers=self.header)
 
     def test_tenant_get(self):
         url = self.base_url + "/mock"
@@ -40,10 +45,21 @@ class TenantTestCase(unittest.TestCase):
         self.assertEqual(json.loads(response.json())['client_secret'], 'mockPasswrod')
 
     def test_tenant_put(self):
-        pass
+        data = {
+            "tenant": "mock",
+            "client_id": "modify",
+            "client_secret": "modify"
+        }
+        response = requests.put(self.base_url, headers=self.header, json=data)
+
+        self.assertEqual(json.loads(response.json())['tenant'], 'mock')
+        self.assertEqual(json.loads(response.json())['client_id'], 'modify')
+        self.assertEqual(json.loads(response.json())['client_secret'], 'modify')
 
     def test_tenant_delete(self):
-        pass
+        url = self.base_url + "/mock"
+        response = requests.delete(url, headers=self.header)
+        self.assertFalse(response.json())
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
