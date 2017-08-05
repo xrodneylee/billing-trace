9db0e7e18120bdd556c3b891c20249c27661fc3f
xrodneylee <xrodneylee@gmail.com>
2017-08-05 17:48:48 +0800

    [test] setting_test : TenantTestCase : : unittest

--- test/settings_test.py
+++ test/settings_test.py
@@ -1,18 +1,43 @@
 import unittest
+import json
+import requests
 
 class TenantTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.base_url = "http://localhost:5000/setting/tenant"
+        self.header = {
+            'Content-Type': 'application/json'
+            }
+        data = {
+            "tenant": "mock",
+            "client_id": "mockId",
+            "client_secret": "mockPasswrod"
+        }
+        response = requests.post(self.base_url, headers=self.header, json=data)
 
     def tearDown(self):
         pass
 
     def test_tenant_post(self):
-        pass
+        data = {
+            "tenant": "guanpu",
+            "client_id": "id",
+            "client_secret": "passwrod"
+        }
+        response = requests.post(self.base_url, headers=self.header, json=data)
+
+        self.assertEqual(json.loads(response.json())['tenant'], 'guanpu')
+        self.assertEqual(json.loads(response.json())['client_id'], 'id')
+        self.assertEqual(json.loads(response.json())['client_secret'], 'passwrod')
 
     def test_tenant_get(self):
-        pass
+        url = self.base_url + "/mock"
+        response = requests.get(url, headers=self.header)
+
+        self.assertEqual(json.loads(response.json())['tenant'], 'mock')
+        self.assertEqual(json.loads(response.json())['client_id'], 'mockId')
+        self.assertEqual(json.loads(response.json())['client_secret'], 'mockPasswrod')
 
     def test_tenant_put(self):
         pass
@@ -21,4 +46,4 @@ class TenantTestCase(unittest.TestCase):
         pass
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
