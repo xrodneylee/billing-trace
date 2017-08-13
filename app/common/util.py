a771acabed58024e14382436b91dc46499a1fa62
xrodneylee <xrodneylee@gmail.com>
2017-08-13 15:27:43 +0800

    [feature] import_data : : : organize tenant and    subscription

---
+++ app/common/util.py
@@ -0,0 +1,14 @@
+from config import tenant_collection, subscription_collection,\
+                   subscription_group_collection, azure_ratecard_collection
+from bson.json_util import dumps
+
+class AzureUtil():
+    
+    @staticmethod
+    def get_all_tenant():
+        return dumps(tenant_collection.find({}, {"_id": 0}))
+
+    @staticmethod
+    def get_all_subscription_by_tenant(tenant):
+        return dumps(subscription_collection.find({"tenant": tenant}, {"subscriptionId": 1,
+                                                                       "_id": 0}))
--- app/scripts/import_data.py
+++ app/scripts/import_data.py
@@ -1,37 +1,38 @@
-from ..services.azure import AzureCredential, AzureRatecard, AzureUsage
-from config import db
-from bson.json_util import dumps
-import configparser
-import requests
+# from ..services.azure import AzureCredential, AzureRatecard, AzureUsage
+import json
+from ..common.util import AzureUtil
+# import configparser
+# import requests
 
-tenants = {}
-subscriptions = {}
-azure_ratecard_collection = db['azure-ratecard']
-tenant_collection = db['tenant']
-subscription_collection = db['subscription']
-subscription_group_collection = db['subscription-group']
+data = []
 
-config = configparser.ConfigParser()
-config.read_file(open("secret.properties"))
+tenants = json.loads(AzureUtil.get_all_tenant())
+for tenant in tenants:
+    subscriptions = json.loads(AzureUtil.get_all_subscription_by_tenant(tenant['tenant']))
+    for subscription in subscriptions:
+        tenant['subscriptions'] = list()
+        tenant['subscriptions'].append(subscription['subscriptionId'])
+    data.append(tenant)
 
-tenant = config.get("Azure", "tenant")
-client_id = config.get("Azure", "client_id")
-client_secret = config.get("Azure", "client_secret")
-subscription = config.get("Azure", "subscription")
-offer_durable_id = config.get("Azure", "offer_durable_id")
+print(data)
+# config = configparser.ConfigParser()
+# config.read_file(open("secret.properties"))
 
-def get_ratecard():
-    credential = AzureCredential(tenant, client_id, client_secret)
-    ratecard = AzureRatecard(credential.invoke().json()['access_token'],
-                             subscription,
-                             offer_durable_id)
-    # for document in ratecard.invoke().json()['Meters']:
-    azure_ratecard_collection.remove({})
-    azure_ratecard_collection.insert(ratecard.invoke().json()['Meters'], check_keys=False)
-    # print(ratecard.invoke().json())
+# tenant = config.get("Azure", "tenant")
+# client_id = config.get("Azure", "client_id")
+# client_secret = config.get("Azure", "client_secret")
+# subscription = config.get("Azure", "subscription")
+# offer_durable_id = config.get("Azure", "offer_durable_id")
 
-def get_tenant():
-    tenants = dumps(tenant_collection.find({}, {"_id": 0}))
-    print(tenants)
+# def get_ratecard():
+#     credential = AzureCredential(tenant, client_id, client_secret)
+#     ratecard = AzureRatecard(credential.invoke().json()['access_token'],
+#                              subscription,
+#                              offer_durable_id)
+#     # for document in ratecard.invoke().json()['Meters']:
+#     azure_ratecard_collection.remove({})
+#     azure_ratecard_collection.insert(ratecard.invoke().json()['Meters'], check_keys=False)
+#     # print(ratecard.invoke().json())
+
 
     
