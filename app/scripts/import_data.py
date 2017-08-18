fd68910c33adacf9bbe7a08a4d5c48ad90d2dc2a
xrodneylee <xrodneylee@gmail.com>
2017-08-19 01:12:40 +0800

    [feature] import_data : : refresh_usage_job :

--- app/scripts/import_data.py
+++ app/scripts/import_data.py
@@ -11,14 +11,14 @@ def refresh_ratecard_job():
 
     for tenant in tenants:
         subscriptions = json.loads(AzureUtil.get_all_subscription_by_tenant(tenant['tenant']))
-        credential = AzureCredential(tenant['tenant'], tenant['client_id'], \
+        credential = AzureCredential(tenant['tenant'], tenant['client_id'],
                                      tenant['client_secret']).invoke().json()['access_token']
         for subscription in subscriptions:
             if subscription['offer_durable_id'] not in offer_durable_id_set:
                 ratecard = AzureRatecard(credential,
                                          subscription['subscriptionId'],
                                          subscription['offer_durable_id'])
-                azure_ratecard_collection.insert(ratecard.invoke().json()['Meters'], \
+                azure_ratecard_collection.insert(ratecard.invoke().json()['Meters'],
                                                  check_keys=False)
                 offer_durable_id_set.add(subscription['offer_durable_id'])
 
@@ -26,7 +26,11 @@ def refresh_usage_job():
     tenants = json.loads(AzureUtil.get_all_tenant())
     for tenant in tenants:
         subscriptions = json.loads(AzureUtil.get_all_subscription_by_tenant(tenant['tenant']))
-        credential = AzureCredential(tenant['tenant'], tenant['client_id'], \
+        credential = AzureCredential(tenant['tenant'], tenant['client_id'],
                                      tenant['client_secret']).invoke().json()['access_token']
         for subscription in subscriptions:
-            pass
+            usage = AzureUsage(credential,
+                               subscription['subscriptionId'],
+                               reported_start_time="DATETIME",
+                               reported_end_time="DATETIME")
+            
--- test/test.py
+++ test/test.py
@@ -1,27 +1,3 @@
-import requests
-import pprint
-import json
-
-url = ' http://127.0.0.1:5000/setting/group'
-header = {
-    'Content-Type': 'application/json'
-}
-# data = {
-#     "tenant": "aaa",
-#     "client_id": "sss",
-#     "client_secret": "fff"
-# }
-
-data = {
-    "groupName": "guanpu",
-    "subscriptions": ["coffee", "tea", "water"]
-}
-# data = '{"groupName": "guanpu", "subscriptions": ["coffee", "tea", "water"]}'
-# print(json.loads(data))
-# response = requests.post(url, headers=header, json=data)
-response = requests.get(url)
-print(response.json())
-# response = requests.get(url)
-# print(response.json())
-# response = requests.delete(url)
-# print(response.json())
+from datetime import datetime, timezone                                
+local_time = datetime.now(timezone.utc).astimezone()
+print(local_time.isoformat())
