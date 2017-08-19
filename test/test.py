1b6df8ea00634bef3bce15111f1df170a4582907
xrodneylee <xrodneylee@gmail.com>
2017-08-19 11:36:27 +0800

    [others] : : : test datetime

--- test/test.py
+++ test/test.py
@@ -1,3 +1,7 @@
 from datetime import datetime, timezone                                
 local_time = datetime.now(timezone.utc).astimezone()
-print(local_time.isoformat())
+# print(local_time)
+# print(local_time.isoformat())
+# print(local_time.date())
+print(str(local_time.date()) + 'T' + str(local_time.hour - 2)+':00:00+08:00')
+# print(local_time)
