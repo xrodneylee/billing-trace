e5f5432eeb55db827218cf5f3feed9649288f3d8
xrodneylee <xrodneylee@gmail.com>
2017-08-29 23:54:52 +0800

    [feature] : : : backend init

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -9,20 +9,23 @@ from .resources.settings import Group
 from .jobs.import_data import refresh_ratecard_job, refresh_usage_job
 
 
-sched = BackgroundScheduler() 
-
 app = Flask(__name__)
 api = Api(app)
 
 
 # restful api
 api.add_resource(Mock, '/mock')
-
-# settings
 api.add_resource(Tenant, '/setting/tenant', '/setting/tenant/<tenant>')
 api.add_resource(Subscription, '/setting/subscription', '/setting/subscription/<subscription>')
 api.add_resource(Group, '/setting/group', '/setting/group/<group_name>')
 
+# scheduler
+sched = BackgroundScheduler()
 sched.add_job(refresh_ratecard_job, 'cron', hour=3)
 sched.add_job(refresh_usage_job, 'cron', minute=40)
 sched.start()
+
+# backend
+from .views import general
+
+app.register_blueprint(general.mod)
