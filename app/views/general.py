861cb96b27dc136506bfa63e07a70bd5a9866507
xrodneylee <xrodneylee@gmail.com>
2017-08-31 00:05:09 +0800

    [enhance] : : : simple login

--- app/views/general.py
+++ app/views/general.py
@@ -3,6 +3,15 @@ from flask import Blueprint, render_template, session, redirect, url_for, \
 
 mod = Blueprint('general', __name__)
 
-@mod.route('/')
+@mod.route('/', methods=['GET'])
+def login():
+    return render_template('general/login.html', warning=request.args.get('warning', 'none'))
+
+@mod.route('/dashboard', methods=['POST'])
 def index():
-    return render_template('general/login.html')
+    user = request.form.get('account')
+    password = request.form.get('password')
+    if user != '@dmin' and password != "P@ssword":
+        return redirect(url_for("general.login", warning="block"))
+    else:
+        return render_template('general/index.html')
