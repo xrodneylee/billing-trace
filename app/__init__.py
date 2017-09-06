16413ca6d2e704f250fb56cd2e810cff4956d5d2
xrodneylee <xrodneylee@gmail.com>
2017-09-06 23:22:54 +0800

    [feature] : : : form submit

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,8 @@ from flask_restful import Api
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 from .resources.mock import Mock
-from .resources.settings import Tenant
-from .resources.settings import Subscription
-from .resources.settings import Group
-from .resources.data import Subscriptions
+from .resources.settings import Tenant, Subscription, Group
+from .resources.data import Subscriptions, HistoryDataImport
 from .jobs.import_data import refresh_ratecard_job, refresh_usage_job
 
 
@@ -20,6 +18,7 @@ api.add_resource(Tenant, '/setting/tenant', '/setting/tenant/<tenant>')
 api.add_resource(Subscription, '/setting/subscription', '/setting/subscription/<subscription>')
 api.add_resource(Group, '/setting/group', '/setting/group/<group_name>')
 api.add_resource(Subscriptions, '/data/<tenant>/subscriptions')
+api.add_resource(HistoryDataImport, '/data/import')
 
 @app.after_request
 def after_request(response):
--- app/resources/data.py
+++ app/resources/data.py
@@ -1,10 +1,20 @@
+from flask import request
 from flask_restful import Resource, reqparse
 from config import tenant_collection, subscription_collection, subscription_group_collection
 from bson.json_util import dumps
 
 class Subscriptions(Resource):
     def get(self, tenant=None):
-        print(tenant)
         return dumps(subscription_collection.find({"tenant": tenant},
                                                   {"subscriptionId": 1,
                                                    "_id": 0}))
+
+class HistoryDataImport(Resource):
+    def post(self):
+        tenant = request.form['tenant']
+        subscription = request.form['subscription']
+        start_date = request.form['startDate']
+        start_time = request.form['startTime']
+        end_date = request.form['endDate']
+        end_time = request.form['endTime']
+        print(tenant, subscription, start_date, start_time, end_date, end_time)
